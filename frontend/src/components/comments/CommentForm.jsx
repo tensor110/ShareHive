8e300c37c33758e5d4ccbe3c289169ec816c23fd
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-06-26 18:10:33 +0530

    addcomments

--- frontend/src/components/comments/CommentForm.jsx
+++ frontend/src/components/comments/CommentForm.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
 
-const CommentForm = ( {formSubmitHandler}) => {
-    const [ value,setValue]= useState("")
-const submithandler =(e)=>{
+const CommentForm = ( {btnLabel,formSubmitHandler}) => {
+    const [ value,setValue]= useState("");
+    
+const submitHandler =(e)=>{
      e.preventDefault()
     formSubmitHandler(value);
     setValue("");
 }
 
   return (
-    <div> 
-        <form onSubmit={submithandler}>
+    
+        <form onSubmit={submitHandler}>
             <div className='flex flex-col items-end border border-primary rounded-lg p-4'>
-                <textarea className='w-full focus:outline-none' rows="5" placeholder='Leave your comment here'
+                <textarea className='w-full focus:outline-none' rows="5" placeholder='Leave your comment here...'
                 value={value}
-                onChange={(e)=> setValue(e.target.value)}></textarea>
+                onChange={(e)=> setValue(e.target.value)} />
                 <button type='submit' className='px-6 py-2.5 rounded-lg bg-primary text-white font-semibold mt-2'>
-                     Send
+                     {btnLabel}
                 </button>
             </div>
         </form>
-    </div>
+    
   )
 }
 
--- frontend/src/components/comments/CommentsContainer.jsx
+++ frontend/src/components/comments/CommentsContainer.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCommentsData } from '../../data/comments'
 import CommentForm from './CommentForm'
 
 const CommentsContainer = ({className}) => {
-    const addCommentHandler = (value,parent = null,replyOnuser = null)
+    const [comments,setComments] = useState([]);
+    console.log(comments);
+    useEffect(()=> {
+    
+        // const getComment = ()=>{}
+        // getComment
+
+        (async()=>{
+            const commentData = await getCommentsData();
+            setComments(commentData)
+        })()
+    }, [])
+
+    const addCommentHandler = (value, parent = null, replyOnuser = null) =>{
+        const newComment = {
+            _id:"10",
+            user:{
+                _id:"a",
+                name:"Venkat",
+            },
+            desc: value,
+            post: "1",
+            parent:parent,
+            replyOnUser: replyOnuser,
+            createdAt: "2024-06-26T17:22:05.092+0000",
+        };
+     setComments((curState)=>{
+        return [newComment, ...curState];
+     });
+    };
   return (
     <div className= {`${className}`}> 
-         <CommentForm btnLable={"btn"}/>
+         <CommentForm btnLabel="Send" formSubmitHandler={(value)=> addCommentHandler(value)}/>
   </div>
   )
 }
